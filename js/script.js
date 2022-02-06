2fbfbd61a23882c70dc74ac5ded658280ae3a778
AndreasPlangger <andreas_plangger@hotmail.com>
2022-02-06 23:42:34 +0100

    delete additional templates

--- js/script.js
+++ js/script.js
@@ -32,65 +32,6 @@ for (let i = 0; i < 9; i++) {
     </div>`;
 }
 
-// for (let i = 3; i < 6; i++) {
-
-//     document.getElementById('row2').innerHTML += `
-//     <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-//     <div class="card text-center border border-primary shadow-0" style="background-color:#ffffff;">
-//         <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-//             <div>
-//                 <span class="card-right"><i class="far fa-bookmark"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i
-//                         class="fas fa-ellipsis-v"></i></span><span class="card-left">Task</span>
-//             </div>
-//             <img src="${tasks[i].image}" class="img-fluid" />
-//             <a href="#!">
-//                 <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-//             </a>
-//         </div>
-//         <div class="card-body">
-//             <h5 class="card-title">${tasks[i].taskName}</h5>
-//             <p class="card-text">${tasks[i].description}
-//             </p>
-//             <hr>
-//             <p><i class="fas fa-exclamation-triangle"></i> Priority level: <button type="button" class="increment-btn ${classbt} counter"><span id="${i}">${tasks[i].importance}</span></button></p>
-//             <p><i class="far fa-calendar-alt"></i> Deadline: ${tasks[i].deadline}</p>
-//             <hr>
-//             <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i> Delete</button>
-//             <button type="button" class="btn btn-success"><i class="far fa-check-circle"></i> Done</button>
-//         </div>
-//     </div>
-//     </div>`;
-// }
-// for (let i = 6; i < 9; i++) {
-
-//     document.getElementById('row3').innerHTML += `
-//     <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-//     <div class="card text-center border border-primary shadow-0" style="background-color:#ffffff;">
-//         <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-//             <div>
-//                 <span class="card-right"><i class="far fa-bookmark"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i
-//                         class="fas fa-ellipsis-v"></i></span><span class="card-left">Task</span>
-//             </div>
-//             <img src="${tasks[i].image}" class="img-fluid" />
-//             <a href="#!">
-//                 <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-//             </a>
-//         </div>
-//         <div class="card-body">
-//             <h5 class="card-title">${tasks[i].taskName}</h5>
-//             <p class="card-text">${tasks[i].description}
-//             </p>
-//             <hr>
-//             <p><i class="fas fa-exclamation-triangle"></i> Priority level: <button type="button" class="increment-btn ${classbt} counter"><span id="${i}">${tasks[i].importance}</span></button></p>
-//             <p><i class="far fa-calendar-alt"></i> Deadline: ${tasks[i].deadline}</p>
-//             <hr>
-//             <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i> Delete</button>
-//             <button type="button" class="btn btn-success"><i class="far fa-check-circle"></i> Done</button>
-//         </div>
-//     </div>
-//     </div>`;
-// }
-
 var button = document.getElementsByClassName('increment-btn');
 
 for (let i = 0; i < 9; i++) {
